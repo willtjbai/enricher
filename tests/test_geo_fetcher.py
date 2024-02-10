d73e6b35e45f5bb73462e10cdda656e5bbb0a182
willbai <will.t.j.bai@gmail.com>
2024-02-11 02:07:21 +0800

    Add test case for invalid ip fetch

--- tests/test_geo_fetcher.py
+++ tests/test_geo_fetcher.py
@@ -5,4 +5,8 @@ from enricher.geo_fetcher import get_country_from_ips
 
 class TestGeoFetcher(TestCase):
     def test_get_country_from_ips(self):
+        # Test case for normal ip
         self.assertEqual(get_country_from_ips('104.193.9.182'), ['The Netherlands'], 'Not able to fetch country')
+        # Test case for invalid ip
+        with self.assertRaises(Exception):
+            get_country_from_ips('0.0.0.0')
